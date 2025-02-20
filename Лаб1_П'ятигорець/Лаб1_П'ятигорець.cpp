e15cdbd58f6bb3102a54e1bfe84c44337dd8a0f3
Witch <pyatigorets.tanya@lll.kpi.ua>
2025-02-20 20:20:06 +0200

    Lab 1 complete!

--- Лаб1_П'ятигорець/Лаб1_П'ятигорець.cpp
+++ Лаб1_П'ятигорець/Лаб1_П'ятигорець.cpp
@@ -1,5 +1,4 @@
 ﻿#include <iostream>
-#include <iomanip>
 #include <chrono>
 #include <thread>
 #include <vector>
@@ -14,11 +13,6 @@ int randGen() {
 int NullGen() {
 	return 0;
 }
-void OneThread(vector<int> A, vector<int> B, int k, vector<int>& C) {
-	for (int i = 0; i < A.size(); i++) {
-		C[i] = A[i] - k * B[i];
-	}
-}
 
 void MultyThreadFunc(int start, int end, vector<int> A, vector<int> B, int k, vector<int>& C) {
 	for (int i = start; i < end; i++) {
@@ -26,19 +20,22 @@ void MultyThreadFunc(int start, int end, vector<int> A, vector<int> B, int k, ve
 	}
 }
 
-int main() {
-	vector<int> arraySizes = { 512, 5120, 10240, 51200, 102400 };
-	vector<int> threadNums = { 2, 4, 8, 16, 32, 64, 128 };
+bool Test();
 
+int main() {
+	vector<int> arraySizes = { 512, 5120, 10240, 102400, 1024000};
+	vector<int> threadNums = { 2, 4, 8, 16, 32, 64, 128 };
+	if (Test()) {
+		return 0;
+	}
 	for (const auto& size : arraySizes) {
 		cout << "Array size: " << size << endl;
 		vector<int> arrayA(size);
 		vector<int> arrayB(size);
+		vector<int> arrayC(size);
 		int k = rand() % 10;
 		generate(arrayA.begin(), arrayA.end(), randGen);
 		generate(arrayB.begin(), arrayB.end(), randGen);
-
-		vector<int> arrayC(size);
 		
 		auto start = chrono::high_resolution_clock::now();
 		MultyThreadFunc(0, size, arrayA, arrayB, k, arrayC);
@@ -74,11 +71,46 @@ int main() {
 	return 0;
 }
 
+bool Test() {
+	const int size = 64;
+	vector<int> arrayA(size);
+	vector<int> arrayB(size);
+	vector<int> arrayC(size);
+	vector<int> arrayCpred(size);
+	int k = rand() % 10;
+	generate(arrayA.begin(), arrayA.end(), randGen);
+	generate(arrayB.begin(), arrayB.end(), randGen);
+
+	for (int i = 0; i < size; i++) {
+		arrayCpred[i] = arrayA[i] - k * arrayB[i];
+	}
+
+	int threadsnum = 4;
+	vector<thread> Threads(threadsnum);
+	int rowsForOneThread = size / threadsnum;
+
+	for (int i = 0; i < threadsnum; i++) {
+		int startRow = rowsForOneThread * i;
+		int endRow = startRow + rowsForOneThread;
+
+		Threads.push_back(thread(MultyThreadFunc, startRow, endRow, ref(arrayA), ref(arrayB), k, ref(arrayC)));
+	}
+	for (auto& t : Threads) {
+		if (t.joinable()) {
+			t.join();
+		}
+	}
+	if (arrayC == arrayCpred) {
+		cout << "Everything working!" << endl;
+		return 0;
+	}
+	else {
+		cout << "Arrays are not equal!" << endl;
+		return 1;
+	}
+}
 
 /*for (const auto& i : arrayC) {
 	cout << i << " ";
 }
 cout << endl;*/
-
-//, 5120, 10240, 51200, 102400 
-// , 8, 16, 32, 64, 128 
