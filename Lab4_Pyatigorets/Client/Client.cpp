d2d734275b8c83ca0b9096c1019efe7088690b42
Witch <pyatigorets.tanya@lll.kpi.ua>
2025-04-11 17:45:15 +0300

    lab4 how to send message

---
+++ Lab4_Pyatigorets/Client/Client.cpp
@@ -0,0 +1,58 @@
+﻿#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <chrono>
+
+#pragma comment(lib, "Ws2_32.lib")
+#define PORT 666
+#define SERVERADDR "127.0.0.1"
+using namespace std;
+
+struct Message {
+    uint32_t length;
+    char data[1024];
+};
+
+int main() {
+
+    WSAData wsaData;
+    if (WSAStartup(0x0202, &wsaData)) {
+        printf("Error WSAStartup %d\n", WSAGetLastError());
+        return -1;
+    }
+
+    SOCKET clientSocket;
+    if ((clientSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Error socket %d\n", WSAGetLastError());
+        WSACleanup();
+        return -1;
+    }
+
+    sockaddr_in dest_addr;
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, SERVERADDR, &dest_addr.sin_addr);
+
+    if (connect(clientSocket, (sockaddr*)&dest_addr, sizeof(dest_addr))) {
+        printf("Connect error %d\n", WSAGetLastError());
+        return -1;
+    }
+    cout << "Conection is successful!" << endl;
+
+    string messageToSend = "hello from client";
+    Message sendMessage;
+    sendMessage.length = htonl(messageToSend.length());
+    strncpy_s(sendMessage.data, messageToSend.c_str(), sizeof(sendMessage.data) - 1);
+    sendMessage.data[sizeof(sendMessage.data) - 1] = '\0';
+
+    size_t bytesSent = send(clientSocket, (char*)&sendMessage, sizeof(sendMessage.length) + messageToSend.length(), 0);
+    if (bytesSent == -1) {
+        std::cerr << "Error sending message." << std::endl;
+    }
+
+    cout << "Data sent successfully" << endl;
+
+    closesocket(clientSocket);
+    WSACleanup();
+    return 0;
+}
---
+++ Lab4_Pyatigorets/Server/Server.cpp
@@ -0,0 +1,101 @@
+﻿#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <chrono>
+
+#pragma comment(lib, "Ws2_32.lib")
+#define PORT 666
+using namespace std;
+
+struct Message {
+    uint32_t length;
+    char data[1024];
+};
+
+int main() {
+
+    char buff[10];
+    int temp = 0;
+    int BytesReceived = 0;
+
+    if (WSAStartup(0x0202, (WSAData*)&buff[0])) {
+        printf("Error WSAStartup %d\n", WSAGetLastError());
+        return -1;
+    }
+
+    SOCKET mysocket;
+    if ((mysocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Error socket %d\n", WSAGetLastError());
+        WSACleanup();
+        return -1;
+    }
+
+    sockaddr_in local_addr;
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_port = htons(PORT);
+    local_addr.sin_addr.s_addr = 0;
+
+    if (bind(mysocket, (sockaddr*)&local_addr, sizeof(local_addr))) {
+        printf("Error bind %d\n", WSAGetLastError());
+        closesocket(mysocket);
+        WSACleanup();
+        return -1;
+    }
+
+    if (listen(mysocket, 0x100)) {
+        printf("Error listen %d\n", WSAGetLastError());
+        closesocket(mysocket);
+        WSACleanup();
+        return -1;
+    }
+
+    cout << "Waiting for client..." << endl;
+
+    sockaddr_in client_addr;
+    int client_addr_size = sizeof(client_addr);
+    SOCKET clientSocket = accept(mysocket, (sockaddr*)&client_addr, &client_addr_size);
+
+    Message receivedMessage;
+    int i = 0;
+    while (i==0) {
+        size_t bytesRead = recv(clientSocket, (char*)&receivedMessage.length, sizeof(receivedMessage.length), 0);
+        if (bytesRead == -1) {
+            std::cerr << "Error receiving message length." << std::endl;
+            break;
+        }
+        else if (bytesRead == 0) {
+            std::cout << "Client disconnected." << std::endl;
+            break;
+        }
+
+        receivedMessage.length = ntohl(receivedMessage.length);
+
+        if (receivedMessage.length > sizeof(receivedMessage.data)) {
+            std::cerr << "Received message length exceeds buffer size." << std::endl;
+            break;
+        }
+        bytesRead = recv(clientSocket, receivedMessage.data, receivedMessage.length, 0);
+        if (bytesRead == -1) {
+            std::cerr << "Error receiving message data." << std::endl;
+            break;
+        }
+        else if (bytesRead == 0) {
+            std::cout << "Client disconnected." << std::endl;
+            break;
+        }
+
+        receivedMessage.data[receivedMessage.length] = '\0';
+
+        std::cout << "Received message: " << receivedMessage.data << std::endl;
+
+        i++;
+    }
+
+    cout << "Bytes received: " << BytesReceived << endl;
+
+    closesocket(clientSocket);
+    closesocket(mysocket);
+    WSACleanup();
+    return 0;
+}
+
